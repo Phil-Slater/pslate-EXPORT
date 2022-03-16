7806ccf0f3c731d987d35b62949e18872298c2d2
Phil-Slater <pdsltr@gmail.com>
2022-03-16 11:03:29 -0400

    add heroku url to Order component

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -18,14 +18,14 @@ function Order() {
     }, [])
 
     const fetchOrder = async (orderNumber) => {
-        const order = await axios.get(`http://localhost:8080/order/${orderNumber}`)
+        const order = await axios.get(`https://pslate-export.herokuapp.com/order/${orderNumber}`)
         setOrder(order.data[0])
     }
 
     const products = order && order.line_items.map(product => {
         return <div className='order-button' key={product.id}>
             <h2>{product.title}</h2>
-            <h3>Quantity: {product.quantity !== 1 ? <b className='quantity'> {product.quantity}</b> : product.quantity}</h3>
+            <h3>Quantity: {product.quantity !== 1 ? <b className='quantity'>{product.quantity}</b> : product.quantity}</h3>
             <p>Build instructions: {product.instructions}</p>
             <p>{product.crimps ? product.crimps : null}</p>
             <p>{product.doubles ? product.doubles : null}</p>
